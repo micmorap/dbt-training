14e32efe9b91855e08ed7c726b0389c299478863
Michael Andrés Mora P <michael.morapp@gmail.com>
2022-08-25 22:14:44 +0000

    20220825 Mike: added delivery team seed and added to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -24,3 +24,6 @@ left join {{ ref('raw_customers') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_products') }} as p 
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
+
